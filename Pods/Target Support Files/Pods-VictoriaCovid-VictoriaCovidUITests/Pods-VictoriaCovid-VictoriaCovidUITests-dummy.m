18befb398c50aa3953534c056f0eabe5e2fd3489
Naveen <naveenarumugam@nbnco.com.au>
2020-07-30 19:58:54 +1000

    Install pod in the application

---
+++ Pods/Target Support Files/Pods-VictoriaCovid-VictoriaCovidUITests/Pods-VictoriaCovid-VictoriaCovidUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VictoriaCovid_VictoriaCovidUITests : NSObject
+@end
+@implementation PodsDummy_Pods_VictoriaCovid_VictoriaCovidUITests
+@end
---
+++ Pods/Target Support Files/Pods-VictoriaCovidTests/Pods-VictoriaCovidTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VictoriaCovidTests : NSObject
+@end
+@implementation PodsDummy_Pods_VictoriaCovidTests
+@end
