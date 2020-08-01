01571c46fef634be1236444aced49db830a16c98
Naveen <naveenarumugam@nbnco.com.au>
2020-08-01 23:38:05 +1000

    Integrate wiremock and AfNetworking

---
+++ VictoriaCovid/ViewModel/MapViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewModel.swift
+//  VictoriaCovid
+//
+//  Created by Naveenprabhu Arumugam on 1/8/20.
+//  Copyright © 2020 Naveenprabhu Arumugam. All rights reserved.
+//
+
+import Foundation
